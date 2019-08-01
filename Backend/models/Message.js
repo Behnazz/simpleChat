15234f88025cbcd57e695c8a4be5dc3dc184b409
Behnaz <behnaz.pourafshari@gmail.com>
2019-08-01 19:21:20 +0100

    change the model

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  name: {
+  nameInput: {
     type: String
   },
-  message: {
+  messageInput: {
     type: String
   }
 });
